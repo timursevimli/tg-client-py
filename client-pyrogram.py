01941d67859401e037ca82afd901237a68882b84
svmlitimur <svmlitimur@gmail.com>
2024-06-06 21:15:15 +0300

    Rename with client prefix

--- client-pyrogram.py
+++ client-pyrogram.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from telethon import TelegramClient, events, utils
+from pyrogram.client import Client
 import asyncio
 import os
 import threading
@@ -7,6 +7,7 @@ import websocket
 import datetime
 import ssl
 import json
+from cachetools import TTLCache
 
 load_dotenv()
 
@@ -24,12 +25,36 @@ if (API_ID == 0 or API_HASH == '' or API_KEY == ''):
     msg = 'Please set environment variables'
     raise Exception(msg)
 
+
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 ignored_channels = config["ignored_channels"]
 
 connection_open_event = threading.Event()
+
+class SessionManager:
+    def __init__(self):
+        self.session = 'first'
+
+    def update_session(self):
+        if self.session == 'first':
+            self.session = 'second'
+        else:
+            self.session = 'first'
+
+    def get_session(self):
+        session = self.session
+        self.update_session()
+        return "sessions/" + session
+
+
+async def destroy(app):
+    await asyncio.sleep(1)
+    async for _ in app.get_dialogs():
+        pass
+    await app.stop()
+
 
 def convert_to_ms(timestamp):
     return int(timestamp * 1000)
@@ -42,14 +67,11 @@ def parse_chat_id(number):
     return number
 
 
-def parse_message(event) -> dict | None:
-    chat_id = event.chat.id
-    message_id = event.id
-    date = event.date
-    try:
-        message = event.text or event.caption
-    except:
-        message = None
+def parse_message(message_data) -> dict | None:
+    chat_id = message_data.chat.id
+    message_id = message_data.id
+    date = message_data.date
+    message = message_data.text or message_data.caption
 
     if message is None:
         return None
@@ -117,27 +139,59 @@ async def get_ws():
     return ws
 
 
+def generate_key(channel_id, message_id): 
+    return str(channel_id) + str(message_id)
+
+
+class ClientManager:
+    def __init__(self, socket):
+        self.session_manager = SessionManager()
+        self.socket = socket
+        self.prev = None
+        self.cache = TTLCache(maxsize=CACHE_MAXSIZE, ttl=CACHE_TTL)
+
+    def init_listeners(self, app):
+        @app.on_message()
+        async def _(_, message):
+            message_object = parse_message(message)
+            if (message_object is None):
+                return
+            channel_id = message_object["channelId"]
+            if (channel_id in ignored_channels):
+                return
+            message_id = message_object["messageId"]
+            key = generate_key(channel_id, message_id)
+            if (self.cache.get(key) is not None):
+                # print("cache hit!!!")
+                return;
+            self.cache[key] = True
+            asyncio.create_task(ws_send_message(self.socket, message_object))
+            # print(message_object)
+
+    async def get_client(self):
+        session = self.session_manager.get_session()
+        app = Client(session, api_id=API_ID, api_hash=API_HASH)
+        self.init_listeners(app)
+        await app.start()
+        if (self.prev is not None):
+            # print("reconnecting!")
+            asyncio.create_task(destroy(self.prev))
+        self.prev = app
+        return app
+
+
 async def main():
     socket = await get_ws()
-    session = 'sessions/telethon'
-    client = TelegramClient(session, API_ID, API_HASH)
-    await client.start()
-    print('Client started!')
-
-    @client.on(events.NewMessage())
-    async def _(event):
-        # print(event)
-        message_object = parse_message(event)
-        if (message_object is None):
-            return
-        channel_id = message_object["channelId"]
-        if (channel_id in ignored_channels):
-            return
-        asyncio.create_task(ws_send_message(socket, message_object))
-        print(message_object)
-
-    await client.run_until_disconnected()
-
+    while True:
+        try:
+            client_manager = ClientManager(socket)
+            while True:
+                await client_manager.get_client()
+                await asyncio.sleep(SESSION_INTERVAL)
+        except Exception as e:
+            print(f'An error ocurred: {e}')
+            await asyncio.sleep(RESTART_INTERVAL)
+            continue
 
 if __name__ == "__main__":
     try:
